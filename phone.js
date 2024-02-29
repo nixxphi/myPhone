5fb1d266c2e46fbf2c11b9b58c11e0f13133f816
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-02-29 10:34:04 -0800

    lets see if this one works

--- phone.js
+++ phone.js
@@ -2,70 +2,59 @@ const readline = require('readline');
 const fs = require('fs');
 const path = require('path');
 
-// JSON FILE PATHS. FINALLY GOT IT WORKING RIGHT.
 const contactsPath = path.resolve(__dirname, 'contacts.json');
 const callHistoryPath = path.resolve(__dirname, 'call_history.json');
 
-// INITIALIZING INTERFACE
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-//LET US BEGIN
 class Phone {
   constructor() {
-    this.phoneNumbers = new Set();
+    this.contacts = [];
     this.observers = [];
     this.loadContactsFromJSON();
     this.loadCallHistoryFromJSON();
     this.regexPattern = /^(0[0-9]{10}|\+[0-9]{3}[0-9]{10})$/;
   }
 
-  // METHOD TO ADD A NEW PHONE NUMBER
-  addPhoneNumber(phoneNumber) {
+  addPhoneNumber(phoneNumber, name) {
     if (this.regexPattern.test(phoneNumber)) {
-      this.phoneNumbers.add(phoneNumber);
+      this.contacts.push({ name, phoneNumber });
+      this.saveContactsToJSON();
     } else {
       console.log(`Invalid phone number format: ${phoneNumber}`);
     }
   }
 
-  // METHOD TO REMOVE A PHONE NUMBER
   removePhoneNumber(phoneNumber) {
-    this.phoneNumbers.delete(phoneNumber);
+    this.contacts = this.contacts.filter(contact => contact.phoneNumber !== phoneNumber);
+    this.saveContactsToJSON();
   }
 
-  // METHOD TO DIAL A PHONE NUMBER
   dialPhoneNumber(input) {
     let phoneNumber = input;
     let contactName = input;
-    // CHECKS IF INPUT IS A REAL PHONE NUMBER
-    if (!this.regexPattern.test(input)) {
-      // IF NOT, TRIES TO FIND CONTACT NAME. I'LL ADMIT IT, THIS CODE IS A LITTLE REDUNDANT BUT I DON'T WANNA TOPPLE THE HOUSE OF CARDS 
-      const contact = this.contacts.find(c => c.name === input);
-      if (contact) {
-        contactName = contact.name;
-        phoneNumber = contact.phoneNumber;
-      } else {
-        console.log(`Phone number or contact name ${input} not found`);
-        return;
-      }
+
+    const contact = this.contacts.find(c => c.phoneNumber === input);
+    if (contact) {
+      contactName = contact.name;
+      phoneNumber = contact.phoneNumber;
+    } else {
+      console.log(`Phone number ${input} not found in contacts`);
+      return;
     }
 
     console.log(`Dialing ${phoneNumber}`);
-
-    // ADDS CALL TO CALL HISTORY SO YOUR BAE CAN CATCH YOU
     this.addCallToHistory(phoneNumber);
     this.notifyObservers(phoneNumber, "Now Dialing");
   }
 
-  // THE METHOD FOR ADDING AN OBSERVER
   addObserver(observer) {
     this.observers.push(observer);
   }
 
-  // THE METHOD FOR REMOVING AN OBSERVER
   removeObserver(observer) {
     const index = this.observers.indexOf(observer);
     if (index !== -1) {
@@ -73,14 +62,12 @@ class Phone {
     }
   }
 
-  // NOTIFYING ALL OBSERVERS, WATCHERS, SNOOPERS AND VOYEURS 
   notifyObservers(phoneNumber, action) {
     for (const observer of this.observers) {
       observer.update(phoneNumber, action);
     }
   }
 
-  // METHOD TO DISPLAY CALL HISTORY... I LEFT OUT THE OPTION TO DELETE CALL HISTORY INTENTIONALLY. CHOP YOUR OWN BREAKFAST LIKE A MAN.
   displayCallHistory() {
     console.log("Call History:");
     for (const phoneNumber of this.callHistory) {
@@ -88,229 +75,175 @@ class Phone {
     }
   }
 
-  // METHOD TO SAVE CONTACTS TO JSON
   saveContactsToJSON() {
     fs.writeFileSync(contactsPath, JSON.stringify(this.contacts, null, 2));
   }
 
-  // METHOD TO LOAD CONTACTS FROM JSON 
   loadContactsFromJSON() {
     if (fs.existsSync(contactsPath)) {
       this.contacts = JSON.parse(fs.readFileSync(contactsPath, 'utf8'));
     } else {
-      this.contacts = [];
-      this.saveContactsToJSON();
+      fs.writeFileSync(contactsPath, '[]');
     }
   }
 
-  // METHOD TO SAVE CALL HISTORY TO JSON
   saveCallHistoryToJSON() {
     fs.writeFileSync(callHistoryPath, JSON.stringify(this.callHistory, null, 2));
   }
 
-  // METHOD TO LOAD CALL HISTORY FROM JSON
   loadCallHistoryFromJSON() {
     if (fs.existsSync(callHistoryPath)) {
       this.callHistory = JSON.parse(fs.readFileSync(callHistoryPath, 'utf8'));
     } else {
-      this.callHistory = [];
-      this.saveCallHistoryToJSON();
+      fs.writeFileSync(callHistoryPath, '[]');
     }
   }
 
-  // METHOD TO ADD CALL TO HISTORY
   addCallToHistory(phoneNumber) {
     this.callHistory.push({ phoneNumber, timestamp: new Date().toISOString() });
     this.saveCallHistoryToJSON();
   }
-}
 
-class Contacts extends Phone {
-  constructor() {
-    super();
-  }
-
-  // METHOD TO ADD A NEW CONTACT
-  addContact(name, phoneNumber) {
-    this.contacts.push({ name, phoneNumber });
-    // NOTIFY OBSERVERS... DEFINITELY REMINDS ME OF THE WATCHERS
-    this.notifyObservers("Added");
-    // SAVE CONTACTS TO JSON
-    this.saveContactsToJSON();
-  }
-
-  // METHOD TO EDIT AN EXISTING CONTACT
+  // NEW EDIT CONTACT FUNCTION
   editContact(name, newPhoneNumber) {
     const contact = this.contacts.find(c => c.name === name);
     if (contact) {
       contact.phoneNumber = newPhoneNumber;
-      // NOTIFIES WATCHERS... ERR OBSERVERS
-      this.notifyObservers("Edited");
-      // SAVE CONTACTS TO JSON
       this.saveContactsToJSON();
     } else {
       console.log(`Contact ${name} not found`);
-    }
-  }
-
-  // TO DELETE A CONTACT
-  removeContact(name) {
-    const index = this.contacts.findIndex(c => c.name === name);
-    if (index !== -1) {
-      this.contacts.splice(index, 1);
-      this.notifyObservers("Deleted");
-      // SAVE CONTACTS TO JSON
-      this.saveContactsToJSON();
-    } else {
-      console.log(`Contact ${name} not found`);
-    }
-  }
-
-  // TO DISPLAY ALL CONTACTS... I'LL HAVE TO ADD A HIDdEN CONTACTS SECTION LATER LOL
-  displayContacts() {
-    console.log("Contacts:");
-    for (let i = 0; i < this.contacts.length; i++) {
-      console.log(`${i + 1}: ${this.contacts[i].name}: ${this.contacts[i].phoneNumber}`);
     }
   }
 }
 
-// CREATING OBSERVER 1
-class Observer1 {
+class Observer {
   constructor(phone) {
     this.phone = phone;
     this.phone.addObserver(this);
   }
 
-  // UPDATE OBSERVER 1
   update(phoneNumber, action) {
-    console.log(`Observer1: ${phoneNumber}`);
+    console.log(`Now Dialing ${phoneNumber}`);
   }
 }
 
-// CREATING CLASS FOR OBERVER 2
 class Observer2 {
   constructor(phone) {
     this.phone = phone;
     this.phone.addObserver(this);
   }
 
-  //UPDATE OBSERVER 2
   update(phoneNumber, action) {
     const last10Digits = phoneNumber.slice(-10);
     console.log(`Observer2: Now Dialing ${last10Digits}`);
   }
 }
 
-// TO DIAL A PHONE NUMBER
-function dialPhoneNumber(phone) {
-  rl.question("Enter phone number: ", (input) => {
-    phone.dialPhoneNumber(input);
-    // GO BACK
-    mainMenu();
-  });
-}
-
-// FOR ADDING A NEW CONTACT
-function addContact(contacts, phone) {
+function addContact(phone) {
   rl.question("Enter contact name: ", (name) => {
     rl.question("Enter contact phone number(080 or +234 format): ", (phoneNumber) => {
-      contacts.addContact(name, phoneNumber);
-      phone.addPhoneNumber(phoneNumber);
+      phone.addPhoneNumber(phoneNumber, name);
       console.log(`New contact ${name} saved.`);
-      // LETS GO BACK AGAIN
-      mainMenu();
+      mainMenu(phone);
     });
   });
 }
 
-// FOR VIEWING CONTACTS ON THE INTERFACE.
-function viewContacts(contacts, phone) {
-  contacts.displayContacts();
+function removePhoneNumber(phone) {
+  rl.question("Enter phone number to remove: ", (input) => {
+    phone.removePhoneNumber(input);
+    mainMenu(phone);
+  });
+}
+
+function dialPhoneNumber(phone) {
+  rl.question("Enter phone number: ", (input) => {
+    const contact = phone.contacts.find(c => c.phoneNumber === input);
+    if (contact) {
+      phone.dialPhoneNumber(input);
+    } else {
+      console.log(`Phone number ${input} not found in contacts`);
+    }
+    mainMenu(phone);
+  });
+}
+
+function displayCallHistory(phone) {
+  phone.displayCallHistory();
+  mainMenu(phone);
+}
+
+// UPDATED VIEW CONTACTS FUNCTION
+function viewContacts(phone) {
+  phone.displayContacts();
   rl.question("Enter the index of the contact you want to interact with: ", (index) => {
     index = parseInt(index) - 1;
-    if (index < 0 || index >= contacts.contacts.length) {
+    if (index < 0 || index >= phone.contacts.length) {
       console.log("Invalid contact index");
-      // GO BACK
-      mainMenu();
+      mainMenu(phone);
       return;
     }
 
-    const contact = contacts.contacts[index];
+    const contact = phone.contacts[index];
     rl.question(`Do you want to (1) dial ${contact.name}, (2) edit ${contact.name}, (3) remove ${contact.name}, or (4) view call history? `, (answer) => {
       switch (answer) {
         case '1':
           phone.dialPhoneNumber(contact.phoneNumber);
-          // SNAP BACK TO THE PREVIOUS MENU
-          mainMenu();
+          mainMenu(phone);
           break;
         case '2':
           rl.question("Enter new phone number: ", (newPhoneNumber) => {
-            contacts.editContact(contact.name, newPhoneNumber);
+            phone.editContact(contact.name, newPhoneNumber); // CALL THE EDIT FUNCTION HERE
             console.log(`Contact ${contact.name} edited successfully.`);
-            // BACK SLIDE A BIT
-            mainMenu();
+            mainMenu(phone);
           });
           break;
         case '3':
-          contacts.removeContact(contact.name);
+          phone.removePhoneNumber(contact.phoneNumber);
           console.log(`Contact ${contact.name} erased successfully.`);
-          // RETURN TO THE PAST NOW!
-          mainMenu();
+          mainMenu(phone);
           break;
         case '4':
           phone.displayCallHistory();
-          // MOON-WALKING TO THE PREVIOUS MENU
-          mainMenu();
-          break;
-        case '5':
-          // BACK-TRACKING
-          mainMenu();
+          mainMenu(phone);
           break;
         default:
           console.log("Invalid choice");
-          // HEADING BACK
-          mainMenu();
+          mainMenu(phone);
           break;
       }
     });
   });
 }
 
-// HERE'S MY INFAMOUS MAIN MENU... I'VE PASTED THAT ALL OVER THIS THING 
-function mainMenu() {
-  rl.question("What do you want to do? \n(1) Dial phone number, \n(2) View contacts, \n(3) Add new contact, \n(4) View call history: ", (answer) => {
+
+function mainMenu(phone) {
+  rl.question("What do you want to do? \n(1) Add new contact, \n(2) Remove phone number, \n(3) Dial phone number, \n(4) View contacts list, \n(5) View call logs: ", (answer) => {
     switch (answer) {
       case '1':
-        dialPhoneNumber(phone);
+        addContact(phone);
         break;
       case '2':
-        viewContacts(contacts, phone);
+        removePhoneNumber(phone);
         break;
       case '3':
-        addContact(contacts, phone);
+        dialPhoneNumber(phone);
         break;
       case '4':
-        phone.displayCallHistory();
-        // Return to previous menu
-        mainMenu();
+        viewContacts(phone);
         break;
+      case '5':
+          displayCallHistory(phone);
+          break;
       default:
         console.log("Invalid choice");
-        // Return to previous menu
-        mainMenu();
+        mainMenu(phone);
         break;
     }
   });
 }
 
-// AND FINALLY... X SAID: "LET THERE BE PHONE... AND CONTACTS"
 const phone = new Phone();
-const contacts = new Contacts();
-
-// INSTANTIATING PERVY VIEWER. SORRY OBSERVER
-const observer1 = new Observer1(phone);
+const observer = new Observer(phone);
 const observer2 = new Observer2(phone);
-
-// MY MVP MAIN MENU ONE MORE TIME
-mainMenu();
+mainMenu(phone);
